c1d574a2cbb2d2944f62d5fc02bdfaf6a4c39343
tanderson <treveranderson01@gmail.com>
2018-10-26 15:43:29 -0500

    literally fixed the worse bug ever.

--- symbolTable.cpp
+++ symbolTable.cpp
@@ -13,6 +13,10 @@ void push_scope(vector<map<string, id_attrs> > *symTable) {
 }
 
 void symbol_table(parsetree *root, vector<map<string, id_attrs> > *sym_table) {
+  // cout << "top of symbol_table at node: " << nodenames[root -> type] << "\n";
+  // if (sym_table -> front().find("a_0") != sym_table -> front().end()) {
+  //    cout << "top of symbol_table: a_0 in symbol table: " << sym_table -> front().find("a_0") -> second.id_name << "\n";
+  // }
   if (root == NULL) {
     cout << "null root, returning\n";
     return;
@@ -26,7 +30,7 @@ void symbol_table(parsetree *root, vector<map<string, id_attrs> > *sym_table) {
 	symbol_table(root -> children[i], sym_table);
       }
     }
-    break;
+  break;
   case node_external_declaration:
     push_scope(sym_table);
     insert_sym_table(root -> children[0] -> children[1], function, sym_table);
@@ -55,51 +59,72 @@ void symbol_table(parsetree *root, vector<map<string, id_attrs> > *sym_table) {
     sym_table -> pop_back();
     break;
   case node_IDENTIFIER: {
-    id_attrs *atts = in_scope(root -> str_ptr, sym_table);
-    if (atts == NULL) {
+    id_attrs atts = in_scope(root -> str_ptr, sym_table);
+    //cout << "sym_table id_name at node_IDENTIFIER: " << sym_table -> front().find("a_0") -> second.id_name << "\n";
+    if (atts.line == -1) {
       cout << "ERROR: symbol " << root -> str_ptr << " is out of scope at line: " << root -> line << "\n";
     } else {
-      root -> symbol_table_ptr = atts;
-      cout << "root -> stp: " << root -> symbol_table_ptr << "\n";
+      root -> symbol_table_ptr = (id_attrs*) calloc(sizeof(id_attrs), 0);
+      root -> symbol_table_ptr -> it = atts.it;
+      root -> symbol_table_ptr -> line = atts.line;
+      root -> symbol_table_ptr -> id_name = (char*) calloc(256 * sizeof(char), 0);
+      strcpy(root -> symbol_table_ptr -> id_name, atts.id_name);
+      root -> symbol_table_ptr -> value = atts.value;
+      //      cout << "root -> stp: " << root -> symbol_table_ptr << "\n";
     }
   }
     break;
   default: 
     for (int i = 0; i < 10 && root -> children[i] != NULL; i++) {
-      symbol_table(root -> children[i], sym_table);
+        // cout << "at node: " << nodenames[root -> type] << " before recursive call\n";
+	// if (sym_table -> front().find("a_0") != sym_table -> front().end()) {
+	//   cout << "a_0 in symbol table before recursive call: " << sym_table -> front().find("a_0") 
+	//     -> second.id_name << "\n";
+	// }
+	symbol_table(root -> children[i], sym_table);
+
+        // cout << "at node: " << nodenames[root -> type] << " after recursive call\n";
+	// if (sym_table -> front().find("a_0") != sym_table -> front().end()) {
+	//   cout << "a_0 in symbol table after recursive call: " << sym_table -> front().find("a_0") 
+	//     -> second.id_name << "\n";
+	// }
     }
     break;
   }
 }
 
-id_attrs* in_scope(string id, vector<map<string, id_attrs> > *sym_table) {
-  id_attrs *ida = NULL;
+id_attrs in_scope(string id, vector<map<string, id_attrs> > *sym_table) {
   for (vector<map<string, id_attrs> >::iterator t = --sym_table -> end(); t >= sym_table -> begin(); t--) {
-    if (key_exists(id, *t)) {
-      ida = &(t -> find(id) -> second);
+    pair<bool, string> exists = key_exists(id, *t);
+    if (exists.first) {
+      cout << "here\n";
+      id_attrs ret = t -> find(exists.second) -> second;
+      cout << "can i get here\n";
+      return ret;
       break;
     }
   }
-  return ida;
+  id_attrs not_found;
+  not_found.line = -1;
+  return not_found;
 }
 
 bool in_global_scope(string id, vector<map<string, id_attrs> > *sym_table) {
-  return key_exists(id, sym_table -> front());
+  return key_exists(id, sym_table -> front()).first;
 }
 
 bool in_top_scope(string id, vector<map<string, id_attrs> > *sym_table) {
-  return key_exists(id, sym_table -> back());
+  return key_exists(id, sym_table -> back()).first;
 }
 
-bool key_exists(string id, map<string, id_attrs> m) {
-  bool exists = false;
+pair<bool, string> key_exists(string id, map<string, id_attrs> m) {
   for (map<string, id_attrs>::const_iterator it = m.begin(); it != m.end(); it++) {
     if (boost::starts_with(it -> first, id)) {
-      exists = true;
+      return pair<bool, string>(true, it->first);
       break;
     }
   }
-  return exists;
+  return pair<bool, string>(false, "not_found");
 }
 
 void output_map(map<string, id_attrs> m) {
@@ -145,6 +170,7 @@ void process_decl_list(parsetree *root, vector<map<string, id_attrs> > *sym_tabl
     } else {
       insert_sym_table(root -> children[0] -> children[1], integer, sym_table);
     }
+    cout << "sym_table id_name in node_decl: " << sym_table -> front().find("a_0") -> second.id_name << "\n";
 
     if (root -> children[1] -> type == node_i_list) {
       process_decl_list(root -> children[1], sym_table);
@@ -190,20 +216,27 @@ bool id_array(parsetree *node, int startChild) {
 
 void insert_sym_table(parsetree *node, id_type type, vector<map<string, id_attrs> > *sym_table) {
   static int seq_num = 0;
-  char* node_under = strcat((char*) node -> str_ptr, "_");
-  id_attrs atts = {
-    type,
-    node -> line,
-    strcat(node_under, to_string(seq_num++).c_str()),
-    0 //initialize variables to 0
-  };
+
+  char buffer[1024];
+  strcpy(buffer, node -> str_ptr);
+  char* node_under = strcat(buffer, "_");
+
+  id_attrs atts;
+  atts.it = type;
+  atts.line = node -> line;
+  atts.id_name = (char*) calloc(256 * sizeof(char), 0);
+  strcpy(atts.id_name, strcat(node_under, to_string(seq_num++).c_str()));
+  atts.value = 0;
+
   if (in_top_scope(atts.id_name, sym_table)) {
     cout << "ERROR: duplicate declaration of identifier " << atts.id_name << " found on line " << atts.line << "\n"; 
   } else {
-    sym_table -> back().insert(pair<string, id_attrs>(atts.id_name, atts));
+     sym_table -> back().insert(pair<string, id_attrs>(atts.id_name, atts));
+    cout << "sym_table id_name after inserting symbol: " << sym_table -> front().find("a_0") -> second.id_name << "\n";
     cout << "new identifier " << atts.id_name << " with type " << get_id_type_name(atts.it) << " in scope at line: "
 	 << atts.line << "\n";
   }
+  
 }
 
 string get_id_type_name(id_type idt) {
--- symbolTable.h
+++ symbolTable.h
@@ -25,8 +25,8 @@ void process_formal_list(parsetree *root, vector<map<string, id_attrs> > *sym_ta
 void insert_sym_table(parsetree *node, id_type type, vector<map<string, id_attrs> > *sym_table);
 void process_decl_list(parsetree *root, vector<map<string, id_attrs> > *sym_table);
 void symbol_table(parsetree *root, vector<map<string, id_attrs> > *sym_table);
-id_attrs* in_scope(string id, vector<map<string, id_attrs> > *sym_table);
-bool key_exists(string id, map<string, id_attrs> m);
+id_attrs in_scope(string id, vector<map<string, id_attrs> > *sym_table);
+pair<bool, string> key_exists(string id, map<string, id_attrs> m);
 bool in_top_scope(string id, vector<map<string, id_attrs> > *sym_table);
 bool in_global_scope(string id, stack<map<string, id_attrs> > *sym_table);
 string get_id_type_name(id_type idt);
